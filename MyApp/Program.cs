b0b2f432461c41b9cce0ddec0130c0182a0930a3
joelochoazamora2005 <joel.ochoa.zamora@cftmail.cl>
2025-04-25 09:28:54 -0400

    Add files via upload

---
+++ MyApp/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+for(int i=0 ; i<10 ; i++){
+    if((i+1)%2 == ){       
+        Console.WriteLine(i+1 + " Es Par");
+    }else{
+        Console.WriteLine(i+1 + " Es Impar")
+    }else{
+        int numero1 = (int)Console.ReadLine()
+
+Console.WriteLine("Por favor, ingrese un numero: ")
+
+string input = Console.ReadLine();
+    }
+    
+}
+
